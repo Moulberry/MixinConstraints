d5e2edff7e7a8f94866b547bfecb34ef90c457ae
IThundxr <me@ithundxr.dev>
2024-05-15 18:05:40 -0400

    FMLn't, it's just... gone

--- src/main/java/com/moulberry/mixinconstraints/abstraction/FabricAbstractions.java
+++ src/main/java/com/moulberry/mixinconstraints/abstraction/FabricAbstractions.java
@@ -5,8 +5,6 @@ import net.fabricmc.loader.api.FabricLoader;
 import java.lang.invoke.MethodHandles;
 
 public class FabricAbstractions {
-    private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
-
     public static boolean checkDevEnvironment() {
         return FabricLoader.getInstance().isDevelopmentEnvironment();
     }
