1559a68a6eb777ddddb05359e1b64fca1bfd9ef6
Rhys <98863820+rhysdh540@users.noreply.github.com>
2024-07-17 10:50:36 -0400

    add neoforge, sources jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
     mavenCentral()
     maven("https://maven.fabricmc.net/")
     maven("https://repo.spongepowered.org/maven")
+    maven("https://maven.neoforged.net/releases")
     maven("https://jitpack.io")
 }
 
@@ -21,6 +22,10 @@ val forge: SourceSet by sourceSets.creating {
     compileClasspath += sourceSets.main.get().output
 }
 
+val neoforge: SourceSet by sourceSets.creating {
+    compileClasspath += sourceSets.main.get().output
+}
+
 dependencies {
     implementation("org.spongepowered:mixin:0.8.5")
     implementation("org.ow2.asm:asm-tree:9.7")
@@ -28,6 +33,22 @@ dependencies {
     implementation("io.github.llamalad7:mixinextras-common:0.3.6")
 
     "fabricImplementation"("net.fabricmc:fabric-loader:0.15.0")
+
     "forgeImplementation"("net.minecraftforge:fmlloader:1.20.6-50.1.0")
     "forgeImplementation"("net.minecraftforge:fmlcore:1.20.6-50.1.0")
+
+    "neoforgeImplementation"("net.neoforged.fancymodloader:loader:3.0.45")
+}
+
+tasks.jar {
+    from(fabric.output)
+    from(forge.output)
+}
+
+tasks.register<Jar>("sourcesJar") {
+    group = "build"
+    archiveClassifier.set("sources")
+    sourceSets.map { it.allSource }.forEach {
+        from(it)
+    }
 }
