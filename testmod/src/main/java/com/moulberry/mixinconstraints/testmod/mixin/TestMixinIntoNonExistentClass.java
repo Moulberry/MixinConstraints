7216691db6908fbb8fd3033c255808353934f015
Moulberry <james.jenour@protonmail.com>
2025-03-05 07:06:44 +0800

    Add testmod mixin for loading non-existent class with failing annotation

---
+++ testmod/src/main/java/com/moulberry/mixinconstraints/testmod/mixin/TestMixinIntoNonExistentClass.java
@@ -0,0 +1,19 @@
+package com.moulberry.mixinconstraints.testmod.mixin;
+
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+
+@IfModLoaded("doesntexist")
+@Mixin(targets = "com.doesnt.exist.Main")
+@Pseudo
+public class TestMixinIntoNonExistentClass {
+
+    /*
+     * Without the annotation there would be an error message in the logs like:
+     * Error loading class: com/doesnt/exist/Main (java.lang.ClassNotFoundException: com/doesnt/exist/Main)
+     *
+     * This is undesirable, so the @IfModLoaded annotation should prevent the loading early enough for this message to not appear
+     */
+
+}
